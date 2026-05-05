118f7997a0ed96a37ee085aa2e9695f019c205ab
Prakhar Dwivedi <136910948+prakhar9918@users.noreply.github.com>
2026-05-05 20:52:44 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "https://task-manager-one-ashy-16.vercel.app/",
+  origin: "https://task-manager-one-ashy-16.vercel.app",
   credentials: true
 }));
 app.use(express.json());
